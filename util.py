972b6272958562a9bbd2b861e21b1c7ac882d307
lucasb-eyer <lucasb.eyer.be@gmail.com>
2015-03-06 16:45:20 +0100

    Added `check_random_state`.

--- util.py
+++ util.py
@@ -1,5 +1,8 @@
 #!/usr/bin/env python3
 
+import numpy as _np
+import numbers
+
 
 def tuplize(what):
     """
@@ -45,3 +48,25 @@ def batched(batchsize, *args):
     rest = n % batchsize
     if rest != 0:
         yield maybetuple(x[-rest:] for x in args)
+
+
+# Blatantly "inspired" by sklearn, for when that's not available.
+def check_random_state(seed):
+    """
+    Turn `seed` into a `np.random.RandomState` instance.
+
+    - If `seed` is `None`, return the `RandomState` singleton used by `np.random`.
+    - If `seed` is an `int`, return a new `RandomState` instance seeded with `seed`.
+    - If `seed` is already a `RandomState` instance, return it.
+    - Otherwise raise `ValueError`.
+    """
+    if seed is None or seed is _np.random:
+        return _np.random.mtrand._rand
+
+    if isinstance(seed, (numbers.Integral, _np.integer)):
+        return _np.random.RandomState(seed)
+
+    if isinstance(seed, _np.random.RandomState):
+        return seed
+
+    raise ValueError('{!r} cannot be used to seed a numpy.random.RandomState instance'.format(seed))
