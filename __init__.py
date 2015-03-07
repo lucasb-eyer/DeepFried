055a5d043e6085ee456f5281da8430eedad4a122
lucasb-eyer <lucasb.eyer.be@gmail.com>
2015-03-07 23:22:17 +0100

    Standard files __init__ and gitignore.

---
+++ __init__.py
@@ -0,0 +1,10 @@
+#!/usr/bin/env python3
+
+import DeepFried.layers as layers
+import DeepFried.output_layers as output_layers
+import DeepFried.containers as containers
+import DeepFried.costs as costs
+import DeepFried.optim as optim
+import DeepFried.pred as pred
+import DeepFried.util as util
+import DeepFried.augmentation as augmentation
